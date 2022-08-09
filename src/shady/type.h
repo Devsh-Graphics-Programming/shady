0b6643459114bbd44d0ef59ecc883179a9990feb
Hugo Devillers <hugo@xol.io>
2022-08-09 15:33:57 +0200

    started refactoring how qualified types work...

--- src/shady/type.h
+++ src/shady/type.h
@@ -25,12 +25,19 @@ const Type* wrap_multiple_yield_types(IrArena* arena, Nodes types);
 Nodes unwrap_multiple_yield_types(IrArena* arena, const Type* type);
 
 // TODO: revise naming scheme
-const Type* strip_qualifier(const Type* type, DivergenceQualifier* qual_out);
-const Type* without_qualifier(const Type* type);
-DivergenceQualifier get_qualifier(const Type* type);
+//const Type* strip_qualifier(const Type* type, DivergenceQualifier* qual_out);
+//const Type* without_qualifier(const Type* type);
+//DivergenceQualifier get_qualifier(const Type* type);
 
 const Type* derive_fn_type(IrArena* arena, const Function* fn);
 Nodes extract_variable_types(IrArena*, const Nodes*);
 Nodes extract_types(IrArena*, Nodes);
+
+/// Ensures an operand has divergence-annotated type and extracts it
+const Type* extract_operand_type(IrArena*, const Type*);
+bool is_operand_uniform(IrArena*, const Type*);
+void deconstruct_operand_type(IrArena*, const Type*, const Type**, bool* uniformity);
+
+bool contains_qualified_type(const Type* type);
 
 #endif
